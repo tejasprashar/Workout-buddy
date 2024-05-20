722ce087cf4f46e63eb58b77d76e99276ec6c5e4
Tejas Prashar <tejasprashar9@gmail.com>
2024-05-20 18:38:19 +0530

    user routes,controller and model

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
 // express app
 const app = express()
